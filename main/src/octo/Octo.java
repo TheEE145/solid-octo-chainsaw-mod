6125349305e6f1cf9c2f1114c0d2914862055019
TheEE145 <Arhitector2009@gmail.com>
2023-01-23 19:43:07 +0200

    making mod modular

--- main/src/octo/Octo.java
+++ main/src/octo/Octo.java
@@ -1,18 +1,18 @@
-package example;
+package octo;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
-import mindustry.gen.*;
-import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
+import mindustry.game.EventType.*;
 
-public class ExampleJavaMod extends Mod{
+import octo.gen.ModSounds;
+import octo.util.Regions;
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
+import org.jetbrains.annotations.NotNull;
+
+public @NotNull class Octo extends mindustry.mod.Mod {
+    public Octo() {
+        Log.info("Loaded Octo constructor.");
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
@@ -21,16 +21,19 @@ public class ExampleJavaMod extends Mod{
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.image(Regions.getRegion("frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
+
+        //sounds creation / disposing
+        Events.on(FileTreeInitEvent.class, ignored -> ModSounds.load());
+        Events.on(DisposeEvent.class, ignored -> ModSounds.dispose());
     }
 
     @Override
-    public void loadContent(){
-        Log.info("Loading some example content.");
+    public void loadContent() {
+        Log.info("Loading some content.");
     }
-
 }
